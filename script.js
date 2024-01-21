5f678c3e02da7a4d7a7937dfba13b23c6204f75c
jacjanowski <janowskijacob@yahoo.com>
2024-01-21 14:18:57 -0600

    finalizign site

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 gsap.registerPlugin(ScrollTrigger);
 
+$('form').submit(function(e){
+  e.preventDefault();      
+  
+  $.ajax({
+      method: 'POST',
+      url: 'https://formsubmit.co/ajax/da20be16d94ef673ddf44846e133d8d9',
+      dataType: 'json',
+      accepts: 'application/json',
+      data: {
+          name: $("#name").val(),
+          email: $("#email").val(),
+          message: $("#message").val()
+      },
+      success: (data) => {$("#success").css("display", "block"), $("form").css("display", "none")},
+      error: (err) => $("#failure").css("display", "block")
+  });
+})
+
 let timeln = gsap.timeline({
   scrollTrigger: {
     trigger: ".services",
     pin: true,
     pinSpacing: true,
-    start: "left-=120px left",
+    start: "left-=20px left",
     end: "+=2000",
     scrub: 1,
   },
@@ -59,13 +77,31 @@ timeln.to(".card-3", {
   opacity: 1,
 });
 
-timeln.to(".card-3", {});
+timeln.from(".card-4", {
+  xPercent: 75,
+  opacity: 0,
+});
+timeln.addLabel("card4");
+
+timeln.to(
+  ".card-3",
+  {
+    scale: 0.98,
+    xPercent: -0.4,
+    opacity: 0.6,
+  },
+  "-=0.3"
+);
+timeln.to(".card-4", {
+  xPercent: 0,
+  opacity: 1,
+});
 
 const tlZoom = gsap.timeline({
   scrollTrigger: {
     trigger: ".container-zoom",
-    start: "top 40%",
-    end: "bottom center",
+    start: "top 20%",
+    end: "bottom bottom",
     pin: true,
     markers: true,
     pinSpacing: false,
@@ -74,16 +110,16 @@ const tlZoom = gsap.timeline({
 });
 tlZoom
   .to(".circle", {
-    scale: 5,
+    scale: 10,
   })
   .to(
-    "h1",
+    ".get-in-touch",
     {
-      scale: -1,
+      scale: 15,
     },
     0
   )
-  .to([".circle", "h1"], {
+  .to([".circle", ".get-in-touch"], {
     opacity: 0,
   });
 
@@ -92,7 +128,7 @@ $(document).ready(function () {
   //   new simpleParallax(image, {
   //     scale: 1.5,
   //   });
-  $("#header-btn").click(function () {
+  $("#header-btn, .next-button").click(function () {
     // Adjust the speed and offset as needed
     $("html, body").animate(
       {
